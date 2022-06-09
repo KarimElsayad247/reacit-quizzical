dbabc0e0fbcad3f675c5a8cc5ff08da16d3b95fe
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-06-09 07:45:13 +0200

    Report the number of correct answers on finish

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -4,14 +4,47 @@ import Question from "./Question";
 
 export default function GameScreen() {
 
-    const [questions, setQuestion] = useState(data["results"])
+    const [checked, setChecked] = useState(false);
+    const [questions, setQuestions] = useState(data["results"]);
+    const [correctlyChosen, setCorrectlyChosen] = useState(Array(5).fill(false));
+    const [numCorrectAnswers, setNumCorrectAnswers] = useState(0);
 
-    const questionElements = questions.map(q => <Question {...q} />) 
+    const questionElements = questions.map((q, i) => (
+            <Question  
+                {...q} 
+                checked={checked} 
+                id={i}
+                toggleCorrectChoice={toggleCorrectChoice}
+            />
+        )
+    ); 
+
+    function check() {
+        if (!checked) {
+            setChecked(true);
+            setNumCorrectAnswers(correctlyChosen.filter(isChosen => isChosen === true).length)
+        }
+        else { // restart game
+            setChecked(false);
+            setQuestions(data["results"]);
+            setNumCorrectAnswers(0);
+        }
+    }
+
+    // Question components 
+    function toggleCorrectChoice(id, newValue) {
+        setCorrectlyChosen(prevChosen => prevChosen.map((choice, i) => i == id ? newValue : choice))
+    }
+
+    console.log(correctlyChosen);
 
     return (
         <main className="game-screen">
             <div className="questions-container">{questionElements}</div>
-            <button className="check-button">Check answers</button>
+            <div className="footer">
+                {checked && <p>You scored {numCorrectAnswers}/5 correct answers</p>}
+                <button onClick={check} className="check-button">Check answers</button>
+            </div>
         </main>
     );
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // From
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -23,53 +23,63 @@ function shuffle(array) {
 
 export default function Question(props) {
 
-    const [checked, setChecked] = useState(false)
-
     // An array containing all the choices, 3 incorrect and one correct
     const choices = [...props.incorrect_answers, props.correct_answer];
     
     // Shuffle the choices, producing another array of strings
     const choicesShuffled = shuffle(choices);
-    
-    console.log(choicesShuffled);
 
     // Create an object for each choice. Objects button state default to empty strings,
     // they should take on values like "wrong" or "chosen" or "correct"
     // and coloring will happen in css.
     const [choicesState, setChoicesState] = useState(choicesShuffled.map(choice => ({
         text: choice,
-        isChosen: false
+        isChosen: false,
+        isCorrect: choice === props.correct_answer,
+        isWrong: choice !== props.correct_answer
     })));
 
-    console.log(choicesState);
-    
-    
     const choiceButtons = choicesState.map((choice, i) => {
         const classNames = {
             chosen: choice.isChosen ? "chosen" : "", 
+            correct: choice.isCorrect && props.checked ? "correct" : "",
+            wrong: choice.isWrong && props.checked ? "wrong" : "",
+            faint: props.checked ? "faint" : ""
         }
         return (<button 
                     key={i}
                     onClick={() => toggle(i)} 
-                    className={`choice-button ${classNames.chosen}`}
+                    className={`choice-button 
+                                ${classNames.chosen} 
+                                ${classNames.correct} 
+                                ${classNames.wrong}
+                                ${classNames.faint}`}
                 >
                     {choice.text}
                 </button>)
     });
 
-    console.log(choiceButtons)
+    // if (props.checked) {
+    //     if (choicesState.some(choice => choice.isChosen && choice.isCorrect)) {
+    //         props.incrementCorrectAnswers();
+    //     }
+    // }
 
     function toggle(id) {
         // Toggle the clicked button, and clear all other buttons, so it's only
         // one button that has state of "chosen"
-        if (!checked) {
+        if (!props.checked) {
             setChoicesState(prevChoices => prevChoices.map((prevChoiceState, i) => ({
                 ...prevChoiceState,
                 isChosen: i == id ? !prevChoiceState.isChosen : ""
             })));
         }
-        console.log(choicesState[id])
     }
+
+    useEffect(() => {
+        const isCorrectlyChosen = choicesState.some(choice => choice.isChosen && choice.isCorrect);
+        props.toggleCorrectChoice(props.id, isCorrectlyChosen);
+    }, [choicesState]);
 
     return (
         <section className="question">
